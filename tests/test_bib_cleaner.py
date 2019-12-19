42b872901f7c73fc553a443bc6138f80e530d43c
sriki18 <srikiranc@gmail.com>
2019-12-18 16:46:21 -0800

    ADD: Comments to `test_tex_tags`

--- tests/test_bib_cleaner.py
+++ tests/test_bib_cleaner.py
@@ -18,14 +18,22 @@ def tex_contents():
 def test_tex_to_tags(tex_contents):
     """Test tex_to_tags function."""
     found_citations = bib_cleaner.tex_to_tags(tex_contents)
+    # base test
     assert "citation1" in found_citations
+    # multiple, separated by comma, no space
     assert "citation2" in found_citations
     assert "citation3" in found_citations
+    # multiple separated by comma, with space
     assert "citation4" in found_citations
     assert "citation5" in found_citations
+    # with hyphen
     assert "ci-tation6" in found_citations
+    # with underscore
     assert "ci_tation7" in found_citations
+    # multiple separated by comma, with and without space, with hyphen, with underscore
     assert "citation-1" in found_citations
     assert "citation_1" in found_citations
+    assert "citation" in found_citations
+    # end with underscore
     assert "1citation_" in found_citations
     assert "citation" in found_citations
