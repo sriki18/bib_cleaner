e27b659f22500f379c1b3df8402b9d8203e2e3cb
sriki18 <srikiranc@gmail.com>
2019-12-18 16:56:24 -0800

    ADD: `test_bib_cleaner`

--- tests/test_bib_cleaner.py
+++ tests/test_bib_cleaner.py
@@ -15,6 +15,13 @@ def tex_contents():
     return ["tests/data/contents.tex"]
 
 
+@pytest.fixture
+def master_bib():
+    """Master bib file fixture.
+    """
+    return ["tests/data/master.bib"]
+
+
 def test_tex_to_tags(tex_contents):
     """Test tex_to_tags function."""
     found_citations = bib_cleaner.tex_to_tags(tex_contents)
@@ -36,4 +43,30 @@ def test_tex_to_tags(tex_contents):
     assert "citation" in found_citations
     # end with underscore
     assert "1citation_" in found_citations
-    assert "citation" in found_citations
+
+
+def test_get_minimal_bib(master_bib, tex_contents):
+    # Check all used
+    all_tags = sorted(bib_cleaner.tex_to_tags(tex_contents))
+    _, n_used, n_tot, _ = bib_cleaner.get_minimal_bib(master_bib[0], all_tags)
+    assert n_used == 12
+    assert n_tot == 12
+    # Check only one used
+    new_tags = all_tags[:1]
+    cont, n_used, n_tot, _ = bib_cleaner.get_minimal_bib(master_bib[0], new_tags)
+    assert n_used == len(new_tags)
+    assert n_tot == 12
+    # Check some used, some not used
+    for each in new_tags:
+        assert each in cont
+    new_tags = all_tags[:6]
+    cont, n_used, n_tot, _ = bib_cleaner.get_minimal_bib(master_bib[0], new_tags)
+    assert n_used == len(new_tags)
+    assert n_tot == 12
+    # Verify used tags exist in contents
+    for each in new_tags:
+        assert each in cont
+    # Verify unused tags don't exist in contents
+    unused_tags = list(set(all_tags) - set(new_tags))
+    for each in unused_tags:
+        assert each not in cont
