2dc63444799f53c5b0afa2e745cab322c8b4d3ec
sriki18 <srikiranc@gmail.com>
2019-12-23 18:34:54 -0800

    Bump version: 0.3.2 → 0.3.3

--- bib_cleaner/__init__.py
+++ bib_cleaner/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Srikiran Chandrasekaran"""
 __email__ = "srikiranc@gmail.com"
-__version__ = "0.3.2"
+__version__ = "0.3.3"
--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sriki18/bib_cleaner",
-    version="0.3.2",
+    version="0.3.3",
     zip_safe=False,
 )
